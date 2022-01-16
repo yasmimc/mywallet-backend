8ac4fc239cbacb7dc7d4dd0afca89dccc8f5c95a
Yasmim Cavalcanti <yasmimcavalcanti.r@gmail.com>
2022-01-16 18:35:38 -0300

    Add .env.test file

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,19 +1,25 @@
 import pg from "pg";
+import dotenv from "dotenv";
 
+dotenv.config();
 const { Pool } = pg;
 
-const user = "postgres";
-const password = "123456";
-const host = "localhost";
-const port = 5432;
-const database = "my-wallet";
-
-const connection = new Pool({
-	user,
-	password,
-	host,
-	port,
-	database,
-});
+const config =
+    process.env.NODE_ENV === "test"
+        ? {
+              user: process.env.DB_USER,
+              password: process.env.DB_PASSWORD,
+              host: process.env.DB_HOST,
+              port: process.env.DB_HOST,
+              database: process.env.DB_NAME,
+          }
+        : {
+              connectionString: process.env.DATABASE_URL,
+              ssl: {
+                  rejectUnauthorized: false,
+              },
+          };
+const connection = new Pool(config);
+console.log(config);
 
 export default connection;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+import "./setup";
 import app from "./app.js";
 
-app.listen(4000, () => {
-	console.log("Server running on port 4000");
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
 });
